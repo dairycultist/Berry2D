a7944d9dbc8c1bde7d139e7951a0ee43739b8638
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-08-07 21:44:20 -0400

    Update Berry2D.h

--- Berry2D.h
+++ Berry2D.h
@@ -1,5 +1,5 @@
-// draw_sprite(x, y, texture_data, sprite_w, sprite_h)
-// draw_text(string, x, y, texture_data, glyph_w, glyph_h)
+#ifndef BERRY2D
+#define BERRY2D
 
 typedef struct {
 
@@ -9,12 +9,20 @@ typedef struct {
 
 } Sprite;
 
+#endif
+
 /*
  * Framework-side implementation
  */
 Sprite *load_sprite(const char *path);
 void draw_sprite(Sprite *sprite, int x, int y);
 void free_sprite(Sprite *sprite);
+
+// draw_sprite(x, y, texture_data, sprite_w, sprite_h)
+// draw_text(string, x, y, texture_data, glyph_w, glyph_h)
+
+// Tileset *load_tileset(const char *path, int tiles_wide, int tiles_high)
+// void draw_tileset(Tileset *tileset, int width, int height, char *tiles)
 
 /*
  * Game-side implementation
