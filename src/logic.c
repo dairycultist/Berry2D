18c5f626dfea373c1bad2d772f7a6ac49375076d
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-09-23 00:13:40 -0400

    Pausing

--- src/logic.c
+++ src/logic.c
@@ -4,6 +4,8 @@
 // edit_level.c
 // main_menu.c (has level selection and stuff)
 
+static bool paused = FALSE;
+
 void init() {
 
 	init_level();
@@ -11,6 +13,15 @@ void init() {
 
 void process(unsigned long time, int input) {
 
-    process_level(time, input);
+	if (JUST_PRESSED(MENU, input)) {
+		paused = !paused;
+	}
+
+	if (!paused)
+    	process_level(time, input);
+
 	draw_level(time, input);
+
+	if (paused)
+		draw_text(font, "PAUSED", WIDTH / 2 - 18, HEIGHT / 2 - 3);
 }
